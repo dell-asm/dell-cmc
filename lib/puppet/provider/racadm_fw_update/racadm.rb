1db7838d98a1572b61f058ae3116e6d877283a05
logan kimmel <logan@lkimmel.com>
2014-08-12 21:35:42 +0000

    Update trigger working, wait failing

--- lib/puppet/provider/racadm_fw_update/racadm.rb
+++ lib/puppet/provider/racadm_fw_update/racadm.rb
@@ -3,7 +3,7 @@ Puppet::Type.type(:racadm_fw_update).provide(:racadm) do
 
   def exists?
     current_version = get_current_version(resource[:fw_version])
-    1 != 0
+    current_version
   end 
 
   def get_current_version(fw_version)
@@ -19,27 +19,45 @@ Puppet::Type.type(:racadm_fw_update).provide(:racadm) do
         version = l.split(" ")[1]
       end
     end
-      if version != fw_version
-        Puppet.debug "Update needed! current version: #{version} | required version #{fw_version}"
-        false
-      else
-        Puppet.debug "CMC fw version up to date"
-        true
-      end
+    if version != fw_version
+      Puppet.debug "Update needed! current version: #{version} | required version #{fw_version}"
+      false
+    else
+      Puppet.debug "CMC fw version up to date"
+      true
+    end
   end
 
   def transport
     if Facter.value(:url) then
-      Puppet.debug "Puppet::Util::NetworkDevice::F5: connecting via facter url."
+      Puppet.debug "Puppet::Util::NetworkDevice::Racadm: connecting via facter url."
       @device ||= Puppet::Util::NetworkDevice::Racadm::Device.new(Facter.value(:url))
     else
       @device ||= Puppet::Util::NetworkDevice.current
-      raise Puppet::Error, "Puppet::Util::NetworkDevice::F5: device not initialized #{caller.join("\n")}" unless @device
+      raise Puppet::Error, "Puppet::Util::NetworkDevice::Racadm: device not initialized #{caller.join("\n")}" unless @device
     end
     @device.transport.connect
   end
   
-  def update
+  def create
+    update_cmd = "racadm fwupdate -g -u -a 172.18.4.100 -d firmimg.cmc -m cmc-standby"
+    begin
+      output = transport.exec!(update_cmd)
+      Puppet.debug "#{output}"
+      if output.include? "failed"
+        raise Puppet::Error, "Puppet::Racadm::Fw_update: failed #{output}"
+      end
+    rescue Puppet::ExecutionFailure => e
+      Puppet.debug("#racadm_fw_update had an error -> #{e.inspect}")
+    end
+    status = transport.exec!("racadm fwupdate -s")
+    Puppet.debug "#{status}"
+    complete = false
+    until complete
+      sleep 10
+      complete = get_current_version(resource[:fw_version])
+    end
   end
-
+    
 end
+
