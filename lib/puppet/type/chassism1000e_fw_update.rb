8cf2924e2ad3ccfb30afe00d2f1279cbace3eb1a
logan kimmel <logan@lkimmel.com>
2014-09-09 16:04:46 +0000

    Validation
    Added validation to check IP and for the file before running any puppet
    commands

--- lib/puppet/type/chassism1000e_fw_update.rb
+++ lib/puppet/type/chassism1000e_fw_update.rb
@@ -14,11 +14,21 @@ Puppet::Type.newtype(:chassism1000e_fw_update) do
   end
 
   newparam(:asm_hostname) do
-    desc "The host ip for the remote location of the firmware"
+    desc "The ip address for the remote location of the firmware"
+    validate do |value|
+      unless value =~ /^(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$/
+        raise ArgumentError, "%s is not a valid IP Address" % value
+      end
+    end
   end
 
   newparam(:path) do
-    desc "The path for the remote location of the firmware"
+    desc "The path to the remote location of the firmwre (on the network share)"
+    validate do |value|
+      unless File.exist? value
+        raise ArgumentErrorm, "The path: %x does not exist" % value
+      end
+    end
   end
 
 end
